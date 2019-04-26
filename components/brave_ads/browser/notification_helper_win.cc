ad6054d692eb4f0429696aa3c3ce3e2c109a4f20
Terry Mancey <terry.mancey@icloud.com>
2019-04-26 16:41:18 +0100

    Fix user earns while notifications are suppressed

---
+++ components/brave_ads/browser/notification_helper_win.cc
@@ -0,0 +1,26 @@
+/* Copyright (c) 2019 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "brave/components/brave_ads/browser/notification_helper_win.h"
+
+namespace brave_ads {
+
+NotificationHelperWin::NotificationHelperWin() = default;
+
+NotificationHelperWin::~NotificationHelperWin() = default;
+
+bool NotificationHelperWin::IsNotificationsAvailable() const {
+  return true;
+}
+
+NotificationHelperWin* NotificationHelperWin::GetInstance() {
+  return base::Singleton<NotificationHelperWin>::get();
+}
+
+NotificationHelper* NotificationHelper::GetInstance() {
+  return NotificationHelperWin::GetInstance();
+}
+
+}  // namespace brave_ads
