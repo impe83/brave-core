3807574992740751d9dbbd03765eae65384880b0
Pranjal Jumde <pranjal.jumde@gmail.com>
2019-05-07 22:58:12 -0700

    lint

--- browser/profiles/brave_profile_manager_unittest.cc
+++ browser/profiles/brave_profile_manager_unittest.cc
@@ -1,10 +1,12 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
+/* Copyright (c) 2019 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include "brave/browser/profiles/brave_profile_manager.h"
 
 #include <string>
+#include <memory>
 
 #include "base/files/file_path.h"
 #include "base/files/scoped_temp_dir.h"
@@ -58,7 +60,6 @@ class BraveProfileManagerTest : public testing::Test {
     ASSERT_TRUE(temp_dir_.CreateUniqueTempDir());
     TestingBrowserProcess::GetGlobal()->SetProfileManager(
         new TorUnittestProfileManager(temp_dir_.GetPath()));
-
   }
 
   void TearDown() override {
@@ -191,3 +192,4 @@ TEST_F(BraveProfileManagerTest, CreateProfilesAsync) {
 
   content::RunAllTasksUntilIdle();
 }
+
